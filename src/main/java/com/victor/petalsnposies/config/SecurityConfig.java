697ed57bfdecf5b952b5831705132deff2670151
Victor Tran <victortran16@gmail.com>
2026-06-01 15:34:44 -0700

    Change security file, added url for approved cors

--- src/main/java/com/victor/petalsnposies/config/SecurityConfig.java
+++ src/main/java/com/victor/petalsnposies/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.victor.petalsnposies.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +31,16 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http
+		 .cors(cors -> cors.configurationSource(request -> {
+	            var config = new org.springframework.web.cors.CorsConfiguration();
+	            config.setAllowedOrigins(List.of(
+	                "http://localhost:5173",
+	                "https://petals-n-posies.vercel.app/"
+	            ));
+	            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+	            config.setAllowedHeaders(List.of("*"));
+	            return config;
+	        }))
         .csrf(csrf -> csrf.disable())
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
