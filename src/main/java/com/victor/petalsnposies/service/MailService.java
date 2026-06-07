796a9120f56e5bf4898eeb7c07997cc54cdaa749
Victor Tran <victortran16@gmail.com>
2026-06-07 12:08:03 -0700

    rework emailService

--- src/main/java/com/victor/petalsnposies/service/MailService.java
+++ src/main/java/com/victor/petalsnposies/service/MailService.java
@@ -1,63 +1,44 @@
 package com.victor.petalsnposies.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.victor.petalsnposies.controller.FlowerController;
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
 import com.victor.petalsnposies.dto.EmailRequestDTO;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-
 @Service
 public class MailService {
-
-	@Autowired
-	private JavaMailSender mailSender;
-//
-//    MailService(JavaMailSender mailSender) {
-//        this.mailSender = mailSender;
-//    }
+	
+	@Value("${resend.api.key}")
+	String resendPass;
+	
+	
 
 	public void sendPlainText(EmailRequestDTO emailRequest) {
-		SimpleMailMessage message = new SimpleMailMessage();
+		Resend resend = new Resend(resendPass);
+		CreateEmailOptions params = CreateEmailOptions.builder()
+				.from("Petals N Posies <onboarding@resend.dev>")
+				.to("victortran16@gmail.com")
+				.subject("Wedding Event")
+				.text("Email: " + emailRequest.getEmail() + "\n" +
+		                  "Name: " + emailRequest.getName() + "\n" +
+		                  "Event Date: " + emailRequest.getEventDate() + "\n" +
+		                  "Venue: " + emailRequest.getVenue() + "\n" +
+		                  "Message: " + emailRequest.getMessage())
+		.build();
 		
-//		message.setTo(emailRequest.getTo());
-//		message.setSubject(emailRequest.getSubject());
-//		message.setText(emailRequest.getBody());
-		String body =  "Email: " + emailRequest.getEmail() +"\n";
-		body = body + "Name: "+ emailRequest.getName() + "\n";
+				
+		 	try {
+	            CreateEmailResponse data = resend.emails().send(params);
+	            System.out.println(data.getId());
+	        } catch (ResendException e) {
+	            e.printStackTrace();
+	        }
+				
 		
-		body = body + "Event Date: "+ emailRequest.getEventDate() + "\n";
+	}
 	
-		body = body + "Venue : "+ emailRequest.getVenue()+ "\n";
-		
-		body = body + "Message: "+ emailRequest.getMessage() + "\n";
-		
-		message.setText(body);
-		message.setFrom("victortran16@gmail.com");
-		message.setTo("victortran16@gmail.com");
-		System.out.println(body);
-		try {
-		    mailSender.send(message);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
 
-	
-		
-	}
-	
-	public void sendHtml(String to, String subject, String htmlBody) throws MessagingException{
-//		MimeMessage message = mailSender.createMimeMessage();
-//		MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
-//		helper.setTo(to);
-//		helper.setSubject(subject);
-//		helper.setText(htmlBody,true);
-//		mailSender.send(message);
-		
-	}
 }
