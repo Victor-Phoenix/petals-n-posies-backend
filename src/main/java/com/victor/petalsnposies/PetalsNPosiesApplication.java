f7f29722f17f4825dbe91134734fdadb79075411
Phong Thien <victortran16@gmail.com>
2026-06-22 14:18:37 -0700

    Removed Helper functions

--- src/main/java/com/victor/petalsnposies/PetalsNPosiesApplication.java
+++ src/main/java/com/victor/petalsnposies/PetalsNPosiesApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetalsNPosiesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("DB_USERNAME=[" + System.getenv("DB_USERNAME") + "]");
-		System.out.println("DB_PASSWORD length=" + System.getenv("DB_PASSWORD").length());
 		SpringApplication.run(PetalsNPosiesApplication.class, args);
 	}
 
